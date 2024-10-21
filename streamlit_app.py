672f62e131f79c42163a8c1f34fd010443120585
Eric Agustin Gutierrez <75638143+ericsiage@users.noreply.github.com>
2024-10-21 20:38:16 +0000

    search-index issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ df = pd.read_excel(r"Datos No llame - actual.xlsx")
 
 # Verifica si el DataFrame tiene datos antes de graficar
 if not df.empty:
-    df[['Numeros']]
+    st.dataframe(df[['Numeros']], height=200, width=500,hide_index=True)
 else:
     st.write("El DataFrame está vacío.")
