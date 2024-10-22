e44b5163a9a2c673a56b4ca57ac5325c85658f8b
Eric Agustin Gutierrez <75638143+ericsiage@users.noreply.github.com>
2024-10-22 13:46:19 +0000

    issue datatype - logo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,11 @@ import pandas as pd
 st.write("""
 # App no llame
 """)
+st.logo(image="flowblanco.jpg")
 
 # Especifica la ruta completa del archivo Excel
 df = pd.read_excel(r"Datos No llame - actual.xlsx")
-
+df['Numeros'] = df['Numeros'].astype(str)
 # Verifica si el DataFrame tiene datos antes de graficar
 if not df.empty:
     st.dataframe(df[['Numeros']], height=200, width=500,hide_index=True)
