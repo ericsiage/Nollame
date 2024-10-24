6dc6f99d17ecb00e01e562978f63093e4e38f31d
Eric Agustin Gutierrez <75638143+ericsiage@users.noreply.github.com>
2024-10-24 14:31:20 +0000

    conexion con googlesheets.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,32 @@
 import streamlit as st
 import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
 st.write("""
 # App - No llame
 """)
-#st.logo(image="flowblanco.jpg")
 
-# Especifica la ruta completa del archivo Excel
-df = pd.read_excel(r"Datos No llame - actual.xlsx")
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+
+df = conn.read()
+#Columna Numeros - str datatype
 df['Numeros'] = df['Numeros'].astype(str)
-# Verifica si el DataFrame tiene datos antes de graficar
+# Print results.
 if not df.empty:
     st.dataframe(df[['Numeros']], height=200, width=500,hide_index=True)
 else:
     st.write("El DataFrame está vacío.")
+
+
+#OLD
+#st.logo(image="flowblanco.jpg")
+# Especifica la ruta completa del archivo Excel
+#df = pd.read_excel(r"Datos No llame - actual.xlsx")
+#df['Numeros'] = df['Numeros'].astype(str)
+# Verifica si el DataFrame tiene datos antes de graficar
+#if not df.empty:
+#    st.dataframe(df[['Numeros']], height=200, width=500,hide_index=True)
+#else:
+#    st.write("El DataFrame está vacío.")
