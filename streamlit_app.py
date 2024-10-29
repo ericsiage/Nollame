b342068c245a36590a70a2ed2a97c744d964c0c0
Eric Agustin Gutierrez <75638143+ericsiage@users.noreply.github.com>
2024-10-29 17:37:20 +0000

    int to str

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,53 +2,47 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
-
-
-st.set_page_config(page_title="No llame",page_icon="flowlogo.PNG",initial_sidebar_state="expanded",layout="wide")
-
+# Configuración de la página
+st.set_page_config(page_title="No llame", page_icon="flowlogo.PNG", initial_sidebar_state="expanded", layout="wide")
 
 st.markdown("""
-        <style>
-            #MainMenu {visibility: hidden;} 
-            footer {visibility: hidden;}
-            .stAppDeployButton {visibility: hidden;}
-            [data-testid="stToolbar"] {visibility: hidden !important;}
-            .css-1yi6l {display: none}
-            header {visibility: hidden;}
+    <style>
+        #MainMenu {visibility: hidden;} 
+        footer {visibility: hidden;}
+        .stAppDeployButton {visibility: hidden;}
+        [data-testid="stToolbar"] {visibility: hidden !important;}
+        .css-1yi6l {display: none}
+        header {visibility: hidden;}
+    </style>
+""", unsafe_allow_html=True)
 
-        </style>
-        """, unsafe_allow_html=True)
+# Título
+st.title("App - No llame")
 
-
-#Title
-st.title("""  App - No llame
-""")
-
-# Create a connection object.
+# Crear objeto de conexión
 conn = st.connection("gsheets", type=GSheetsConnection)
-
-
 df = conn.read()
-#Columna Numeros - str datatype
 
-telefonos = df["Numeros"].astype(str)
+# Columna Numeros - convertir a tipo str sin .0
+telefonos = df["Numeros"].astype(int).astype(str)
 
-valor = ""
-busqueda = st.text_input(label="Busca un número sin 0 adelante,sin espacios en blanco y sin código país. (ejemplo: 99123456)", value=valor, type="default", label_visibility="visible")
+# Input de búsqueda
+busqueda = st.text_input(label="Busca un número sin 0 adelante, sin espacios en blanco y sin código país (ejemplo: 99123456)", value="", label_visibility="visible")
 
-# Verifica si el input está vacío
-if not busqueda:
-    st.write("")
+# Verifica si el input tiene más de 8 caracteres
+if busqueda and len(busqueda) > 8:
+    st.error("Error: La búsqueda debe tener 8 caracteres o menos.")
 else:
-    # Print results.
-    if busqueda in telefonos.values:
-        # Si el número exacto se encuentra, mostrar el mensaje correspondiente
-        st.write("Número encontrado: ", busqueda)
-    else:
-        # Filtrar el DataFrame por el texto ingresado
-        df_filtrado = df[telefonos.str.contains(busqueda, na=False)]
-        df_filtrado = df_filtrado.astype(str)
-        if not df_filtrado.empty:
-            st.dataframe(df_filtrado, height=100, width=500, hide_index=True)
+    # Procesar la búsqueda solo si el input no está vacío
+    if busqueda:
+        # Print results.
+        if busqueda in telefonos.values:
+            st.write("Número encontrado:", busqueda)
         else:
-            st.write("No se encontraron coincidencias.")
+            # Filtrar el DataFrame por el texto ingresado
+            df_filtrado = df[telefonos.str.contains(busqueda, na=False)]
+            df_filtrado = df_filtrado.astype(int).astype(str)
+            if not df_filtrado.empty:
+                st.dataframe(df_filtrado, height=100, width=500, hide_index=True)
+            else:
+                st.write("No se encontraron coincidencias.")
